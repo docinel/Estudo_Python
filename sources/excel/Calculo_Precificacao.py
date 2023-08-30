16d18bd7fea1539d6cfb1b8ed2478220f0421689
Rodrigo Docinel <docinel@gmail.com>
2023-08-29 21:41:22 -0300

    Inicio da precificacao

---
+++ sources/excel/Calculo_Precificacao.py
@@ -0,0 +1,21 @@
+preco_venda = 0
+materia_prima = 0
+comissao_vendas_repres = 0.03
+comissao_gerente_vendas = 0.27
+custo_frete = preco_venda * 0.04
+
+
+'''M.P. + M.E.	                    182,00 60,67%
+COMISSÃO VENDAS REPRESENTANTES 	 0,81 	0,27%
+COMISSÃO GERENTE VENDAS	         -   	0,00%
+CUSTO FRETE FROTA	            6,00 	2,00%
+ICMS	                        27,00 	9,00%
+PIS/COFINS	                    10,95 	3,65%
+IR/CSSL	                        9,18 	3,06%
+SIMPLES	                        -   	0,00%
+ANTECIPAÇÃO BOL. (30/60/90)	    -   	0,00%
+TOTAL DESPESAS VARIÁVEIS	 235,94     78,65%
+		
+MARGEM CONTRIBUIÇÃO	            64,06 	21,35%'''
+
+
---
+++ sources/excel/teste_excel.py
@@ -0,0 +1,21 @@
+import openpyxl as xl
+
+calculo = 1.5
+
+wb = xl.load_workbook(r'sources/excel/calculo.xlsx')
+sheet = wb['Plan1']
+
+for rows in sheet.iter_rows(min_row=2, max_row=3):
+    for cell in rows:
+        print(cell.value, end=" ")
+
+coluna_alvo = "A"
+linha_alvo = 2
+
+celula = f'{coluna_alvo}{linha_alvo}'
+
+valor = sheet[celula].value
+wb.close()
+
+print(valor * calculo)
+
