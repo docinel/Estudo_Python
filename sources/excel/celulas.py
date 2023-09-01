5168b217185bdf0abc8996c53a89c71b40a56c8d
Rodrigo Docinel <docinel@gmail.com>
2023-09-01 16:09:04 -0300

    Quase la

---
+++ sources/excel/celulas.py
@@ -0,0 +1,70 @@
+import openpyxl as xl
+
+wb = xl.load_workbook(r'sources/excel/calculo.xlsx')
+ws = wb.active
+
+meu_dic = {'A2': ws['A2'].value, 'B2': ws['B2'].value,
+           'A3': ws['A3'].value, 'B3': ws['B3'].value,
+           'A4': ws['A4'].value, 'B4': ws['B4'].value,
+           'A5': ws['A5'].value, 'B5': ws['B5'].value,
+           'A6': ws['A6'].value, 'B6': ws['B6'].value,
+           'A7': ws['A7'].value, 'B7': ws['B7'].value,
+           'A8': ws['A8'].value, 'B8': ws['B8'].value,
+           'A9': ws['A9'].value, 'B9': ws['B9'].value,
+           'A10': ws['A10'].value, 'B10': ws['B10'].value,
+           'A11': ws['A11'].value, 'B11': ws['B11'].value,
+           'A12': ws['A12'].value, 'B12': ws['B12'].value,
+           'A13': ws['A13'].value, 'B13': ws['B13'].value,
+           'A14': ws['A14'].value, 'B14': ws['B14'].value,
+           'A15': ws['A15'].value, 'B15': ws['B15'].value,
+           'A16': ws['A16'].value, 'B16': ws['B16'].value
+           }
+
+cont = 2
+while cont < 12:
+    print(meu_dic[f'A{cont}'])
+    print(meu_dic[f'B{cont}'])
+    cont +=1   
+
+# 
+# 
+# A5 = ws['A5'].value
+# B5 = ws['B5'].value
+# A6 = ws['A6'].value
+# B6 = ws['B6'].value
+# A7 = ws['A7'].value
+# B7 = ws['B7'].value
+# A8 = ws['A8'].value
+# B8 = ws['B8'].value
+# A9 = ws['A9'].value
+# B9 = ws['B9'].value
+# A10 = ws['A10'].value
+# B10 = ws['B10'].value
+
+# A2 = ws['A2'].value
+# B2 = ws['B2'].value
+# A3 = ws['A3'].value
+# B3 = ws['B3'].value
+# A4 = ws['A4'].value
+# B4 = ws['B4'].value
+# A5 = ws['A5'].value
+# B5 = ws['B5'].value
+# A6 = ws['A6'].value
+# B6 = ws['B6'].value
+# A7 = ws['A7'].value
+# B7 = ws['B7'].value
+# A8 = ws['A8'].value
+# B8 = ws['B8'].value
+# A9 = ws['A9'].value
+# B9 = ws['B9'].value
+# A10 = ws['A10'].value
+# B10 = ws['B10'].value
+
+
+
+
+
+
+
+
+
--- sources/excel/ler_excel.py
+++ sources/excel/ler_excel.py
@@ -3,12 +3,22 @@ import openpyxl as xl
 wb = xl.load_workbook(r'sources/excel/calculo.xlsx')
 ws = wb.active
 
+teste=0
+while teste < 20:
+    teste1 = teste
+    teste +=1
 # print(wb.sheetnames)
-for row in ws.iter_rows(min_row=2, values_only=True):
-    for cell in row:
-        print(cell)
-
-
+# for row in ws.iter_rows(min_row=2, values_only=True):
+#     for cell in row:
+#         print(cell)
+#for row in ws.iter_rows(min_row=2,values_only=True):
+cont = teste1
+qtd_max_cel = int(input('Qtd linhas: '))
+while cont < qtd_max_cel:
+    celula = ws[f'A{teste1}']
+    print(celula.value)
+    cont +=1
+    
 # celula = ws['A2']
 # celula2 = ws['B2']
 
