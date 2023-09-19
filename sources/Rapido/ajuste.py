32fb4f832f5e346814b53617c37490670631eaea
Rodrigo Docinel <docinel@gmail.com>
2023-09-18 23:33:19 -0300

    ajuste rapdo

--- sources/Rapido/ajuste.py
+++ sources/Rapido/ajuste.py
@@ -1,13 +1,16 @@
 import pyautogui as pa
 
 pa.sleep(5)
-# inicio = 0
-# while inicio < 16:
-#     pa.press('F2')
-#     pa.hotkey('ctrl', 'left')
-#     pa.sleep(0.5)
-#     pa.press('backspace')
-#     pa.press('enter')
+inicio = 0
+while inicio < 12:
+    pa.press('F2')
+    pa.press('left', presses=3, interval=0.2)
+    pa.press('backspace')
+    pa.typewrite('2')
+    # pa.hotkey('ctrl', 'left')
+    # pa.sleep(0.5)
+    # pa.press('backspace')
+    pa.press('enter')
 #     # *******************************************************************************
 #     # pa.press('F2')
 #     # pa.press('left', presses=9, interval=0.2)
@@ -15,7 +18,7 @@ pa.sleep(5)
 #     # pa.press('left', presses=2, interval=0.2)
 #     # pa.press('backspace')
 #     # pa.press('enter')
-#     inicio += 1
+    inicio += 1
 
 
 # for num in range(1, 6):
